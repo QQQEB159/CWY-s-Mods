9e8da3bee0b8b9b8fa65ca836dfa2b20a175cb20
Maple <168573768+yigezhihuimazidexie@users.noreply.github.com>
2026-02-24 13:12:40 +0800

    Update CreditsState.hx

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -1,279 +1,208 @@
 package states;
 
 import objects.AttachedSprite;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 
 class CreditsState extends MusicBeatState
 {
 	var curSelected:Int = -1;
-
-	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var iconArray:Array<AttachedSprite> = [];
 	private var creditsStuff:Array<Array<String>> = [];
 
 	var bg:FlxSprite;
-	var descText:FlxText;
 	var intendedColor:FlxColor;
-	var descBox:AttachedSprite;
 
-	var offsetThing:Float = -75;
+	// 当前显示卡片的元素
+	var currentIcon:FlxSprite;
+	var currentName:FlxText;
+	var currentRole:FlxText;
+	var roleBox:FlxSprite;
+
+	// 卡片位置常量（可调）
+	var iconX:Float = 0;
+	var iconY:Float = 200;
+	var nameY:Float = 120;
+	var roleY:Float = 370;
+	var boxPadding:Int = 10;
 
 	override function create()
 	{
 		#if DISCORD_ALLOWED
-		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
 		#end
 
 		persistentUpdate = true;
+
+		// 背景
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		add(bg);
 		bg.screenCenter();
-		
-		grpOptions = new FlxTypedGroup<Alphabet>();
-		add(grpOptions);
 
+		// 加载模组 credits
 		#if MODS_ALLOWED
 		for (mod in Mods.parseList().enabled) pushModCreditsToList(mod);
 		#end
 
-		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-			['Mobile Porting Team'],
-			['HomuHomu833',			'homura',             'Head Porter of Psych Engine and Author of linc_luajit-rewriten',                       'https://youtube.com/@HomuHomu833',		'FFE7C0'],
-			['Karim Akra',			'karim',			'Second Porter of Psych Engine',						'https://youtube.com/@Karim0690',		'FFB4F0'],
-			['Moxie',				'moxie',			'Helper of Psych Engine Mobile',							'https://twitter.com/moxie_specalist',  'F592C4'],
-			[''],
-			["Psych Engine Team"],
-			["Shadow Mario",		"shadowmario",		"Main Programmer and Head of Psych Engine",					"https://ko-fi.com/shadowmario",	"444444"],
-			["Riveren",				"riveren",			"Main Artist/Animator of Psych Engine",						"https://x.com/riverennn",			"14967B"],
-			[""],
-			["Former Engine Members"],
-			["bb-panzu",			"bb",				"Ex-Programmer of Psych Engine",							"https://x.com/bbsub3",				"3E813A"],
-			[""],
-			["Engine Contributors"],
-			["crowplexus",			"crowplexus",	"Linux Support, HScript Iris, Input System v3, and Other PRs",	"https://twitter.com/IamMorwen",	"CFCFCF"],
-			["Kamizeta",			"kamizeta",			"Creator of Pessy, Psych Engine's mascot.",				"https://www.instagram.com/cewweey/",	"D21C11"],
-			["MaxNeton",			"maxneton",			"Loading Screen Easter Egg Artist/Animator.",	"https://bsky.app/profile/maxneton.bsky.social","3C2E4E"],
-			["Keoiki",				"keoiki",			"Note Splash Animations and Latin Alphabet",				"https://x.com/Keoiki_",			"D2D2D2"],
-			["SqirraRNG",			"sqirra",			"Crash Handler and Base code for\nChart Editor's Waveform",	"https://x.com/gedehari",			"E1843A"],
-			["EliteMasterEric",		"mastereric",		"Runtime Shaders support and Other PRs",					"https://x.com/EliteMasterEric",	"FFBD40"],
-			["MAJigsaw77",			"majigsaw",			".MP4 Video Loader Library (hxvlc)",						"https://x.com/MAJigsaw77",			"5F5F5F"],
-			["iFlicky",				"flicky",			"Composer of Psync and Tea Time\nAnd some sound effects",	"https://x.com/flicky_i",			"9E29CF"],
-			["KadeDev",				"kade",				"Fixed some issues on Chart Editor and Other PRs",			"https://x.com/kade0912",			"64A250"],
-			["superpowers04",		"superpowers04",	"LUA JIT Fork",												"https://x.com/superpowers04",		"B957ED"],
-			["CheemsAndFriends",	"cheems",			"Creator of FlxAnimate",									"https://x.com/CheemsnFriendos",	"E1E1E1"],
-			[""],
-			["Funkin' Crew"],
-			["ninjamuffin99",		"ninjamuffin99",	"Programmer of Friday Night Funkin'",						"https://x.com/ninja_muffin99",		"CF2D2D"],
-			["PhantomArcade",		"phantomarcade",	"Animator of Friday Night Funkin'",							"https://x.com/PhantomArcade3K",	"FADC45"],
-			["evilsk8r",			"evilsk8r",			"Artist of Friday Night Funkin'",							"https://x.com/evilsk8r",			"5ABD4B"],
-			["kawaisprite",			"kawaisprite",		"Composer of Friday Night Funkin'",							"https://x.com/kawaisprite",		"378FC7"],
-			[""],
-			["Psych Engine Discord"],
-			["Join the Psych Ward!", "discord", "", "https://discord.gg/2ka77eMXDv", "5165F6"]
+		// 默认 credits 列表（请复制你的完整列表）
+		var defaultList:Array<Array<String>> = [
+			["Cwy",		"Cwy",		"director",					"https://space.bilibili.com/3493119353948379?spm_id_from=333.337.0.0",	"444444"],
+			["Baka", "Baka", "Main Programmer","https://www.bilibili.com/video/BV1rs41197Xn/?share_source=copy_web&vd_source=5b936a9bef8d1a986b34aeda2b7ef5fb","#ADD8E6"],
+			["Icon-BF",		"icon-bf",		"programming",					"https://space.bilibili.com/488017110",	"444444"],
+			["XJ-Akers",		"XJ-Akers",		"painting",					"https://space.bilibili.com/1684262832",	"444444"],
+			["Star Fison",		"Star Fison",		"arrangement",					"https://space.bilibili.com/515522141",	"444444"],
+			["woofwolf",		"woofwolf",		"painting",					"https://space.bilibili.com/514249942?spm_id_from=333.1387.follow.user_card.click",	"444444"],
+			["Join us!", "QQ", "1038989304", "https://space.bilibili.com/3493119353948379?spm_id_from=333.337.0.0", "5165F6"]
 		];
-		
-		for(i in defaultList)
-			creditsStuff.push(i);
-	
-		for (i => credit in creditsStuff)
-		{
-			var isSelectable:Bool = !unselectableCheck(i);
-			var optionText:Alphabet = new Alphabet(FlxG.width / 2, 300, credit[0], !isSelectable);
-			optionText.isMenuItem = true;
-			optionText.targetY = i;
-			optionText.changeX = false;
-			optionText.snapToPosition();
-			grpOptions.add(optionText);
+		for (i in defaultList) creditsStuff.push(i);
 
-			if(isSelectable)
-			{
-				if(credit[5] != null)
-					Mods.currentModDirectory = credit[5];
+		// 计算图标目标X（屏幕居中，假设图标宽150）
+		iconX = (FlxG.width - 150) / 2;
 
-				var str:String = 'credits/missing_icon';
-				if(credit[1] != null && credit[1].length > 0)
-				{
-					var fileName = 'credits/' + credit[1];
-					if (Paths.fileExists('images/$fileName.png', IMAGE)) str = fileName;
-					else if (Paths.fileExists('images/$fileName-pixel.png', IMAGE)) str = fileName + '-pixel';
-				}
+		// 设置初始选中项（第一个可选项）
+		curSelected = 0;
+		while (unselectableCheck(curSelected)) curSelected++;
 
-				var icon:AttachedSprite = new AttachedSprite(str);
-				if(str.endsWith('-pixel')) icon.antialiasing = false;
-				icon.xAdd = optionText.width + 10;
-				icon.sprTracker = optionText;
-	
-				// using a FlxGroup is too much fuss!
-				iconArray.push(icon);
-				add(icon);
-				Mods.currentModDirectory = '';
+		// 创建第一个卡片（无动画，方向任意）
+		buildCard(creditsStuff[curSelected], false, 1);
 
-				if(curSelected == -1) curSelected = i;
-			}
-			else optionText.alignment = CENTERED;
-		}
-		
-		descBox = new AttachedSprite();
-		descBox.makeGraphic(1, 1, FlxColor.BLACK);
-		descBox.xAdd = -10;
-		descBox.yAdd = -10;
-		descBox.alphaMult = 0.6;
-		descBox.alpha = 0.6;
-		add(descBox);
-
-		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
-		descText.scrollFactor.set();
-		//descText.borderSize = 2.4;
-		descBox.sprTracker = descText;
-		add(descText);
-
+		// 背景颜色
 		bg.color = CoolUtil.colorFromString(creditsStuff[curSelected][4]);
 		intendedColor = bg.color;
-		changeSelection();
 
 		addTouchPad('UP_DOWN', 'A_B');
 
 		super.create();
 	}
 
-	var quitting:Bool = false;
-	var holdTime:Float = 0;
-	override function update(elapsed:Float)
+	// 根据数据构建卡片，animated 决定是否播放进入动画，direction 决定滑动方向（1=从右向左，-1=从左向右）
+	function buildCard(data:Array<String>, animated:Bool, direction:Int)
 	{
-		if (FlxG.sound.music.volume < 0.7)
+		// 图标
+		var iconName:String = data[1];
+		var iconPath:String = 'credits/missing_icon';
+		if (iconName != null && iconName.length > 0)
 		{
-			FlxG.sound.music.volume += 0.5 * elapsed;
+			var testPath = 'credits/' + iconName;
+			if (Paths.fileExists('images/$testPath.png', IMAGE)) iconPath = testPath;
+			else if (Paths.fileExists('images/$testPath-pixel.png', IMAGE)) iconPath = testPath + '-pixel';
+		}
+		currentIcon = new FlxSprite(iconX, iconY).loadGraphic(Paths.image(iconPath));
+		currentIcon.antialiasing = !iconPath.endsWith('-pixel') && ClientPrefs.data.antialiasing;
+		currentIcon.setGraphicSize(150, 150);
+		currentIcon.updateHitbox();
+		add(currentIcon);
+
+		// 名字 (vcr.ttf)
+		currentName = new FlxText(0, nameY, 0, data[0], 40);
+		currentName.setFormat(Paths.font("vcr.ttf"), 40, FlxColor.WHITE, CENTER);
+		currentName.screenCenter(X);
+		add(currentName);
+
+		// 职位（如果有）
+		var roleText:String = (data[2] != null && data[2].length > 0) ? data[2] : null;
+		if (roleText != null)
+		{
+			currentRole = new FlxText(0, roleY, 0, roleText, 24);
+			currentRole.setFormat(Paths.font("DM.ttf"), 24, FlxColor.BLACK, CENTER);
+			currentRole.screenCenter(X);
+
+			// 背景框
+			var boxWidth:Int = Std.int(currentRole.width + boxPadding * 2);
+			var boxHeight:Int = Std.int(currentRole.height + boxPadding);
+			roleBox = new FlxSprite().makeGraphic(boxWidth, boxHeight, 0xCCADD8E6); // 淡蓝半透明
+			roleBox.screenCenter(X);
+			roleBox.x = (FlxG.width - boxWidth) / 2;
+			roleBox.y = roleY - currentRole.height / 2 - boxPadding / 2+20;
+			add(roleBox);
+			add(currentRole);
 		}
 
-		if(!quitting)
+		// 如果播放进入动画
+		if (animated)
 		{
-			if(creditsStuff.length > 1)
-			{
-				var shiftMult:Int = 1;
-				if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
+			// 设置初始位置在屏幕外
+			var startX = iconX + direction * FlxG.width;
+			currentIcon.x = startX;
+			currentIcon.alpha = 0;
+			currentName.alpha = 0;
+			if (currentRole != null) currentRole.alpha = 0;
+			if (roleBox != null) roleBox.alpha = 0;
 
-				var upP = controls.UI_UP_P;
-				var downP = controls.UI_DOWN_P;
+			// 图标滑动
+			FlxTween.tween(currentIcon, {x: iconX, alpha: 1}, 0.4, {ease: FlxEase.sineInOut});
 
-				if (upP)
-				{
-					changeSelection(-shiftMult);
-					holdTime = 0;
-				}
-				if (downP)
-				{
-					changeSelection(shiftMult);
-					holdTime = 0;
-				}
-
-				if(controls.UI_DOWN || controls.UI_UP)
-				{
-					var checkLastHold:Int = Math.floor((holdTime - 0.5) * 10);
-					holdTime += elapsed;
-					var checkNewHold:Int = Math.floor((holdTime - 0.5) * 10);
-
-					if(holdTime > 0.5 && checkNewHold - checkLastHold > 0)
-					{
-						changeSelection((checkNewHold - checkLastHold) * (controls.UI_UP ? -shiftMult : shiftMult));
-					}
-				}
-			}
-
-			if(controls.ACCEPT && (creditsStuff[curSelected][3] == null || creditsStuff[curSelected][3].length > 4)) {
-				CoolUtil.browserLoad(creditsStuff[curSelected][3]);
-			}
-			if (controls.BACK)
-			{
-				FlxG.sound.play(Paths.sound('cancelMenu'));
-				MusicBeatState.switchState(new MainMenuState());
-				quitting = true;
-			}
+			// 名字和职位淡入（延迟一点）
+			FlxTween.tween(currentName, {alpha: 1}, 0.3, {startDelay: 0.1});
+			if (currentRole != null) FlxTween.tween(currentRole, {alpha: 1}, 0.3, {startDelay: 0.1});
+			if (roleBox != null) FlxTween.tween(roleBox, {alpha: 1}, 0.3, {startDelay: 0.1});
 		}
-		
-		for (item in grpOptions.members)
-		{
-			if(!item.bold)
-			{
-				var lerpVal:Float = Math.exp(-elapsed * 12);
-				if(item.targetY == 0)
-				{
-					var lastX:Float = item.x;
-					item.screenCenter(X);
-					item.x = FlxMath.lerp(item.x - 70, lastX, lerpVal);
-				}
-				else
-				{
-					item.x = FlxMath.lerp(200 + -40 * Math.abs(item.targetY), item.x, lerpVal);
-				}
-			}
-		}
-		super.update(elapsed);
 	}
 
-	var moveTween:FlxTween = null;
+	// 销毁当前卡片
+	function destroyCard()
+	{
+		if (currentIcon != null) { remove(currentIcon); currentIcon.destroy(); }
+		if (currentName != null) { remove(currentName); currentName.destroy(); }
+		if (currentRole != null) { remove(currentRole); currentRole.destroy(); }
+		if (roleBox != null) { remove(roleBox); roleBox.destroy(); }
+	}
+
+	// 切换人物
 	function changeSelection(change:Int = 0)
 	{
-		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		do
-		{
-			curSelected = FlxMath.wrap(curSelected + change, 0, creditsStuff.length - 1);
-		}
-		while(unselectableCheck(curSelected));
+		if (change == 0) return;
 
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		// 保存旧选中索引，用于判断方向
+		var oldSelected = curSelected;
+
+		// 找到下一个可选的索引
+		do {
+			curSelected = FlxMath.wrap(curSelected + change, 0, creditsStuff.length - 1);
+		} while (unselectableCheck(curSelected));
+
+		// 计算滑动方向：1 表示下一个（从右向左），-1 表示上一个（从左向右）
+		var direction = (curSelected > oldSelected) ? 1 : -1;
+
+		// 销毁旧卡片
+		destroyCard();
+
+		// 构建新卡片并播放动画
+		buildCard(creditsStuff[curSelected], true, direction);
+
+		// 更新背景颜色
 		var newColor:FlxColor = CoolUtil.colorFromString(creditsStuff[curSelected][4]);
-		//trace('The BG color is: $newColor');
-		if(newColor != intendedColor)
+		if (newColor != intendedColor)
 		{
 			intendedColor = newColor;
 			FlxTween.cancelTweensOf(bg);
-			FlxTween.color(bg, 1, bg.color, intendedColor);
+			FlxTween.color(bg, 0.5, bg.color, intendedColor);
 		}
+	}
 
-		for (num => item in grpOptions.members)
-		{
-			item.targetY = num - curSelected;
-			if(!unselectableCheck(num)) {
-				item.alpha = 0.6;
-				if (item.targetY == 0) {
-					item.alpha = 1;
-				}
-			}
-		}
-
-		descText.text = creditsStuff[curSelected][2];
-		if(descText.text.trim().length > 0)
-		{
-			descText.visible = descBox.visible = true;
-			descText.y = FlxG.height - descText.height + offsetThing - 60;
-	
-			if(moveTween != null) moveTween.cancel();
-			moveTween = FlxTween.tween(descText, {y : descText.y + 75}, 0.25, {ease: FlxEase.sineOut});
-	
-			descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
-			descBox.updateHitbox();
-		}
-		else descText.visible = descBox.visible = false;
+	// 判断是否为分隔线（不可选）
+	private function unselectableCheck(num:Int):Bool
+	{
+		return creditsStuff[num].length <= 1;
 	}
 
 	#if MODS_ALLOWED
 	function pushModCreditsToList(folder:String)
 	{
 		var creditsFile:String = Paths.mods(folder + '/data/credits.txt');
-		
 		#if TRANSLATIONS_ALLOWED
-		//trace('/data/credits-${ClientPrefs.data.language}.txt');
 		var translatedCredits:String = Paths.mods(folder + '/data/credits-${ClientPrefs.data.language}.txt');
 		#end
 
 		if (#if TRANSLATIONS_ALLOWED (FileSystem.exists(translatedCredits) && (creditsFile = translatedCredits) == translatedCredits) || #end FileSystem.exists(creditsFile))
 		{
 			var firstarray:Array<String> = File.getContent(creditsFile).split('\n');
-			for(i in firstarray)
+			for (i in firstarray)
 			{
 				var arr:Array<String> = i.replace('\\n', '\n').split("::");
-				if(arr.length >= 5) arr.push(folder);
+				if (arr.length >= 5) arr.push(folder);
 				creditsStuff.push(arr);
 			}
 			creditsStuff.push(['']);
@@ -281,7 +210,30 @@ class CreditsState extends MusicBeatState
 	}
 	#end
 
-	private function unselectableCheck(num:Int):Bool {
-		return creditsStuff[num].length <= 1;
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.7)
+			FlxG.sound.music.volume += 0.5 * elapsed;
+
+		if (creditsStuff.length > 1)
+		{
+			var shiftMult:Int = FlxG.keys.pressed.SHIFT ? 3 : 1;
+
+			if (controls.UI_UP_P)
+				changeSelection(-shiftMult);
+			else if (controls.UI_DOWN_P)
+				changeSelection(shiftMult);
+		}
+
+		if (controls.ACCEPT && (creditsStuff[curSelected][3]?.length > 4))
+			CoolUtil.browserLoad(creditsStuff[curSelected][3]);
+
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new MainMenuState());
+		}
+
+		super.update(elapsed);
 	}
 }
